511ed8d0152811809d828ff9d3b0b8c1dcfe9999
Yuri Brandao <claudio.y07@gmail.com>
2018-07-17 21:52:49 -0300

    teste

--- sw.js
+++ sw.js
@@ -17,20 +17,20 @@ self.addEventListener('install', function (event) {
   )
 });
 
-self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', function activator(event) {
+//   event.waitUntil(
+//     caches.keys().then(function (keys) {
+//       return Promise.all(keys
+//         .filter(function (key) {
+//           return key.indexOf(CACHE_NAME) !== 0;
+//         })
+//         .map(function (key) {
+//           return caches.delete(key);
+//         })
+//       );
+//     })
+//   );
+// });
 
 // self.addEventListener('fetch', function (event) {
 //   event.respondWith(
