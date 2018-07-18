bd43e1487369eaf0c914d89467223444f8105f48
Yuri Brandao <claudio.y07@gmail.com>
2018-07-17 21:51:09 -0300

    asd

--- sw.js
+++ sw.js
@@ -9,10 +9,33 @@ self.addEventListener('install', function (event) {
         '/img/git.png',
         '/img/gmail1.png',
         '/img/insta.png',
-        '/img/twitter.png',
+        '/img/.png',
         '/styles.css',
         '/manifest.json',
       ]);
     })
   )
 });
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (cachedResponse) {
+//       return cachedResponse || fetch(event.request);
+//     })
+//   );
+// });
