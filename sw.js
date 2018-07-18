1941622c5655a537b2e950cb2b9b31b6e7fa2d9a
Yuri Brandao <claudio.y07@gmail.com>
2018-07-17 22:06:59 -0300

    teste service

--- sw.js
+++ sw.js
@@ -2,15 +2,10 @@ var CACHE_NAME = 'static-v1';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open('airhorner').then(function (cache) {
       return cache.addAll([
         '/',
         '/index.html',
-        '/img/git.png',
-        '/img/gmail1.png',
-        '/img/insta.png',
-        '/img/twitter.png',
-        '/img/boy256.png',
         '/styles.css',
         '/manifest.json',
       ]);
