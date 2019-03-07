335123d42af1cac1ff10cd0fcf3e1717ef162571
Cláudio Yuri <claudio.y07@gmail.com>
2019-03-07 20:51:41 -0300

    criação da nova exibição de projetos
    foi retirado as imagens foi feita a refatorção do codigo do get pra pegar os repositorios agora os repositorios são salvos no cache do cliente pra nao precisar da get a cada requisição

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,38 @@
 var projects = [];
+var table = document.getElementById('myTable');
 
-$.get("https://api.github.com/users/claudiuri/repos", (data) => {
-
-    data.forEach(element => {
-        let project = { name: element.name, description:  element.description, language: element.language}
-        projects.push(project);
+if(localStorage.getItem('projects') == null){
+    $.get('https://api.github.com/users/claudiuri/repos', (data) => {
+    
+        data.forEach(element => {
+            let project = { name: element.name, description:  element.description, language: element.language, url: element.html_url}
+            projects.push(project);
+        });
+        
+        projects = projects.filter(element => {
+            return element.description != null;
+        });
+        
+        localStorage.setItem('projects', JSON.stringify(projects));
+        
+        insertProjectsInTable(projects);       
     });
+}else{
+    
+    projects = JSON.parse(localStorage.getItem('projects'));
+    insertProjectsInTable(projects);
+}
 
-});
+function insertProjectsInTable(projects){
+    projects.forEach(element => {
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = `<a target="_blank" title="Ver projeto no github" href="${element.url}">${element.name}</a>`;
+        cell2.innerHTML = element.description;
+        cell3.innerHTML = element.language;
+        cell3.className = "d-none d-sm-block";
+    });
+}
 
-projects.forEach(element => {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-})
-
-console.log(projects);
